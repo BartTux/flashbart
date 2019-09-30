54ab0e2d9fe512f448e6b02575f44b7381903e0d
BartTux <bartpot98@tutanota.com>
2019-09-30 10:24:05 +0200

    Supplement flashcard's controller with static data
    Write an array contain static data (data from database in future)
    inside main controller's FlashcardController::showFlashcards() method.
    Also modify mainpage twig template by adding twig functions that decide
    which data from controller should be show on the page.
    
    Install annotations package that allows to write a routes inside the
    annotations above the controller methods. Remove YAML routes and
    replace their with annotation routes.
    
    Also modify a bit the heading styles by adding a cursor pointer style
    for checkboxes in "Show" group.

--- src/Controller/FlashcardController.php
+++ src/Controller/FlashcardController.php
@@ -3,11 +3,46 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class FlashcardController extends AbstractController
 {
+    /**
+     * @Route("/")
+     */
     public function showFlashcards()
     {
-        return $this->render('main.html.twig');
+        $flashcard = [
+            [
+                'word' => 'ratification',
+                'translation' => '',
+                'pronun' => 'ra\'tifikejszyn',
+                'example' => 'It\'s our priev to got ratification.',
+            ], [
+                'word' => 'dog',
+                'translation' => '',
+                'pronun' => '',
+                'example' => 'My dog is brown and very nice.'
+            ], [
+                'word' => 'specific',
+                'translation' => '',
+                'pronun' => 'spe\'syfik',
+                'example' => 'I mean this specific case.'
+            ], [
+                'word' => 'particular',
+                'translation' => '',
+                'pronun' => 'pe\'tikjula',
+                'example' => ''
+            ], [
+                'word' => 'variable',
+                'translation' => '',
+                'pronun' => '',
+                'example' => ''
+            ]
+        ];
+
+        return $this->render('main.html.twig', [
+            'flashcards' => $flashcard
+        ]);
     }
 }
