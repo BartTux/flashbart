036b12934d3fafd6a0ec1fce94dc43daea2d8919
BartTux <bartpot98@tutanota.com>
2019-10-01 10:18:16 +0200

    Add "show a word translation" function
    Add FlashcardController::showFlashcardTranslation method inside the
    FlashcardController that takes a id parameter which is a number of
    the choosen card. Write the route for this function.
    
    Inside the main twig template (main.html.twig) modify the code by
    adding condition which check if id of a card exist in URI (id exists
    when user choose some card or when write the URI with that id by
    oneself). If YES - display only translation of specific flashcard and
    display the rest of the cards by default (foreign word + pronunciation
    and example sentence if choosen in top bar), If NO - display everything
    by default.
    
    Move CSS styles for flashcards to another file (_flashcard.scss) and
    directory (/css/components) and modify the min height for card.

--- src/Controller/FlashcardController.php
+++ src/Controller/FlashcardController.php
@@ -3,46 +3,72 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FlashcardController extends AbstractController
 {
-    /**
-     * @Route("/")
-     */
-    public function showFlashcards()
+    private $flashcard;
+
+    public function __construct()
     {
-        $flashcard = [
+        $this->flashcard = [
             [
+                'id' => 1,
                 'word' => 'ratification',
-                'translation' => '',
+                'translation' => 'ratyfikacja',
                 'pronun' => 'ra\'tifikejszyn',
                 'example' => 'It\'s our priev to got ratification.',
             ], [
+                'id' => 2,
                 'word' => 'dog',
-                'translation' => '',
+                'translation' => 'pies',
                 'pronun' => '',
                 'example' => 'My dog is brown and very nice.'
             ], [
+                'id' => 3,
                 'word' => 'specific',
-                'translation' => '',
+                'translation' => 'konkretny',
                 'pronun' => 'spe\'syfik',
                 'example' => 'I mean this specific case.'
             ], [
+                'id' => 4,
                 'word' => 'particular',
-                'translation' => '',
+                'translation' => 'szczególny',
                 'pronun' => 'pe\'tikjula',
                 'example' => ''
             ], [
+                'id' => 5,
                 'word' => 'variable',
-                'translation' => '',
+                'translation' => 'zmienna',
                 'pronun' => '',
                 'example' => ''
             ]
         ];
+    }
 
+    /**
+     * @Route("/")
+     * @return Response
+     */
+    public function showFlashcards(): Response
+    {
         return $this->render('main.html.twig', [
-            'flashcards' => $flashcard
+            'flashcards' => $this->flashcard,
+            'id' => null
+        ]);
+    }
+
+    /**
+     * @Route("/show/{id}")
+     * @param int $id
+     * @return Response
+     */
+    public function showFlashcardTranslation(int $id): Response
+    {
+        return $this->render('main.html.twig', [
+            'flashcards' => $this->flashcard,
+            'id' => $id
         ]);
     }
 }
