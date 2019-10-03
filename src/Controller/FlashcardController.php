08459584fc21502f83c8267bc2e4a5d942304971
BartTux <bartpot98@tutanota.com>
2019-10-03 10:28:54 +0200

    Change static categories to dynamic

--- src/Controller/FlashcardController.php
+++ src/Controller/FlashcardController.php
@@ -19,55 +19,64 @@ class FlashcardController extends AbstractController
                 'translation' => 'ratyfikacja',
                 'pronun' => 'ra\'tifikejszyn',
                 'example' => 'It\'s our priev to got ratification.',
+                'category' => 'other'
             ], [
                 'id' => 2,
                 'word' => 'dog',
                 'translation' => 'pies',
                 'pronun' => '',
-                'example' => 'My dog is brown and very nice.'
+                'example' => 'My dog is brown and very nice.',
+                'category' => 'animals'
             ], [
                 'id' => 3,
                 'word' => 'specific',
                 'translation' => 'konkretny',
                 'pronun' => 'spe\'syfik',
-                'example' => 'I mean this specific case.'
+                'example' => 'I mean this specific case.',
+                'category' => 'other'
             ], [
                 'id' => 4,
                 'word' => 'particular',
                 'translation' => 'szczególny',
                 'pronun' => 'pe\'tikjula',
-                'example' => ''
+                'example' => '',
+                'category' => 'other'
             ], [
                 'id' => 5,
                 'word' => 'variable',
                 'translation' => 'zmienna',
                 'pronun' => '',
-                'example' => ''
+                'example' => '',
+                'category' => 'other'
             ]
         ];
     }
 
     /**
-     * @Route("/", name="flashcard_index", methods={"GET"})
+     * @Route("/{slug}", name="flashcard_index", methods={"GET"})
+     * @param string $slug
      * @return Response
      */
-    public function showFlashcards(): Response
+    public function showFlashcards(string $slug): Response
     {
         return $this->render('main.html.twig', [
             'flashcards' => $this->flashcard,
+            'category' => $slug,
             'id' => null
         ]);
     }
 
     /**
-     * @Route("/show/{id}", name="flashcard_translation", methods={"GET"})
+     * @Route("/{slug}/show/{id}", name="flashcard_show", methods={"GET"})
+     * @param string $slug
      * @param int $id
      * @return Response
      */
-    public function showFlashcardTranslation(int $id): Response
+    public function showFlashcardTranslation(string $slug, int $id): Response
     {
         return $this->render('main.html.twig', [
             'flashcards' => $this->flashcard,
+            'category' => $slug,
             'id' => $id
         ]);
     }
